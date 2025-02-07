c607281a5ca5c89bff2a19d277bff9ff780a15c2
Victory Ndu <victor5ndu@gmail.com>
2025-02-07 17:24:09 +0100

    refactor: rename 'number' to 's_number' in Counter contract

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+    uint256 public s_number;
 
     function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        s_number = newNumber;
     }
 
     function increment() public {
-        number++;
+        s_number++;
     }
 }
